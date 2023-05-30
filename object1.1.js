ae1cfa7c8744d7dfe2c5c4727d27b09dce3ef472
Izuchii1311 <luthfiramadhan.lr55@gmail.com>
2023-05-30 08:33:26 +0700

    #1 Object 1.1 (Object Literal, Function Declaration, Construct Function)

---
+++ object1.1.js
@@ -0,0 +1,67 @@
+// Cara membuat Object pada Javascript
+// 1. Object Literal
+// let mahasiswa = {
+//     nama: "Luthfi Nur Ramadhan",
+//     energy: 10,
+//     // method adalah function yang ada di dalam obect
+//     makan: function(porsi) {
+//         // di dalam object ada keyword this
+//         this.energy = this.energy + porsi;
+//         console.log(`Selamat Makan ${this.nama}!\nEnergy kamu meningkat sebesar ${porsi}%`);
+//         return; 
+//     }
+//     // mahasiswa
+//     // mahasiswa.makan(2)
+// }
+
+
+
+// 2. Function Declaration
+// Nama Mahasiswa besar untuk menandakan bahwa ini adalah Object
+// function Mahasiswa(nama, energy) {
+//     let mahasiswa = {};
+//     mahasiswa.nama = nama;
+//     mahasiswa.energy = energy;
+
+//     // Method
+//     mahasiswa.makan = function(porsi) {
+//         this.energy = this.energy + porsi;
+//         console.log(`Selamat Makan ${this.nama}!\nEnergy kamu meningkat sebesar ${porsi}%`);
+//     }
+
+//     mahasiswa.main = function(jam){
+//         this.energy = this.energy - jam;
+//         console.log(`Selamat Bermain ${this.nama}!\nEnergy kamu akan berkurang sebesar ${jam}%`);
+//     }
+//     return mahasiswa;
+// }
+
+// // intansiasi
+// let Izuchii = Mahasiswa('Izuchii', 10)
+// let Luthfi = Mahasiswa('Luthfi Nur Ramadhan', 10)
+
+// // Izuchii
+// // Izuchii.makan(3)
+// // Izuchii.main(2)
+
+
+
+// 3. Constructor Function
+// keyword new
+function Mahasiswa(nama, energy) {
+    this.nama = nama;
+    this.energy = energy;
+
+    // Method
+    this.makan = function(porsi) {
+        this.energy = this.energy + porsi;
+        console.log(`Selamat Makan ${this.nama}!\nEnergy kamu meningkat sebesar ${porsi}%`);
+    }
+
+    this.main = function(jam){
+        this.energy = this.energy - jam;
+        console.log(`Selamat Bermain ${this.nama}!\nEnergy kamu akan berkurang sebesar ${jam}%`);
+    }
+}
+
+let Izuchii = new Mahasiswa('Izuchii', 10);
